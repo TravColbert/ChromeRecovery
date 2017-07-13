e80547fc88bfdfd9eabb2d29129f57a92a088100
Travis Colbert <trav.colbert@gmail.com>
2017-07-13 15:51:34 -0700

    Beginning to take some shape

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 var appState;
 var sectionList = [];
 var manufacturerList = {};
-var listImage = document.getElementById("list_image");
-var listManufacturer = document.getElementById("list_manufacturer");
+var listImage, listManufacturer;
 
 var ready = function(fn) {
   if (document.readyState != 'loading'){
@@ -12,6 +11,7 @@ var ready = function(fn) {
   }
 }
 
+/* == Image List == */
 var sortImages = function(a, b) {
   var aM = a.manufacturer.toLowerCase();
   var bM = b.manufacturer.toLowerCase();
@@ -23,35 +23,43 @@ var sortImages = function(a, b) {
   return 0;
 }
 
-var showImages = function() {
-  for(var c=0; c<images.length; c++) {
-    console.log(images[c].manufacturer);
-  }
+var clearImages = function() {
+  listImage.empty();
 }
 
-var displayImages = function() {
+var displayImages = function(filterFunction,argList) {
+  clearImages();
+  if(filterFunction===null || filterFunction===undefined) {
+    filterFunction = function() { return true };
+  }
   for(var c=0; c<images.length; c++) {
-    displayImage(images[c],c);
+    if(filterFunction(images[c],argList)) displayImage(images[c],c);
   }
 }
 
 var displayImage = function(image,id) {
   imgString = image.manufacturer + " " + image.model;
   var img = new Laminar.Widget({
-    element:"p",
     id:"img_" + id,
-    parent: "#list_image",
-  }).update();
+    classlist:["image_entry"],
+    parent: listImage
+  });
   var imgLink = new Laminar.Widget({
     id:"image_link_" + id,
     element:"a",
     content:image.name,
     proplist:[["href",image.url]],
-    parent:"#img_" + id
-  }).update();
+    classlist:["image_link"],
+    parent: img
+  });
 }
 
-/* == Manufacturer == */
+/* == Filter Functions for displaying subsets of the image list == */
+var filterByManufacturer = function(image,manufacturer) {
+  return (image.manufacturer.toLowerCase()==manufacturer.toLowerCase())
+}
+
+/* == Manufacturer List == */
 var buildManufacturerList = function() {
   for(var c=0; c<images.length; c++) {
     var m = images[c].manufacturer;
@@ -80,13 +88,17 @@ var displayManufacturer = function(manufacturerName, id) {
     datalist: {name:manufacturerName,id:id}
   });
   name.listenEvent("click",function(e,obj) {
-    console.log(obj.getData("name") + " clicked");
+    displayImages(filterByManufacturer,obj.getData("name"));
+    setAppState("image");
   });
 }
 
-ready(function() {
-  /* Set up state machines */
-  appState = {'mode':'manufacturer'};
+/* == APP STATE and triggers for modifying it == */
+var setAppState = function(mode) {
+  appState.__set_mode = mode;
+}
+
+var setSectionModeActions = function() {
   var sections = document.querySelectorAll("section");
   /* Make all document sections react to appState */
   for(var c=0; c<sections.length; c++) {
@@ -96,4 +108,35 @@ ready(function() {
       this.setState((this.getData("mode")==v) ? "active" : "inactive");
     }.bind(sect));  /* We have to BIND the function to the current object */
   }
+}
+
+var setSectionTitleActions = function() {
+  var sectionTitles = document.querySelectorAll(".sectiontitle");
+  for(var c=0; c<sectionTitles.length; c++) {
+    var sectionTitle = new Laminar.Widget(sectionTitles[c]);
+    var sectionMode = sectionTitle.getData("mode");
+    if(sectionMode!==undefined) {
+      sectionTitle.listenEvent("click",function(e,obj) {
+        setAppState(obj.getData("mode"));
+      });
+    }
+    var sectionList = new Laminar.Widget({
+      id:"list_" + sectionMode,
+      classlist:["layout-col","sectionlist"],
+      parent:"#select_" + sectionTitle.getData("mode")
+    });
+    if(sectionMode=="image") listImage = sectionList;
+    if(sectionMode=="manufacturer") listManufacturer = sectionList;
+  }
+}
+
+ready(function() {
+  appState = {'mode':'manufacturer'}; // Set default app state
+  images.sort(sortImages);  // Sort images retrieved from JSON
+  setSectionModeActions();  // Make the sections reactive based on app state
+  setSectionTitleActions(); // When you click on the section title change the app state
+  appState.__set_mode = "manufacturer";
+  buildManufacturerList();
+  displayManufacturerList();
+  displayImages();
 })
